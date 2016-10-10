36b93170be6de5636bd8457e1fb5c2f5c654e35f
Michal Povinsky <antihutka@gmail.com>
2016-10-10 13:44:04 +0200

    Implemented load_prev for extr.lua

--- extr.lua
+++ extr.lua
@@ -5,9 +5,8 @@ local cmd = torch.CmdLine()
 cmd:option('-out', 'valloss')
 cmd:option('-json', 'cv/mora140-3-gridgru-1/_4000.json')
 cmd:option('-avg', 1)
-
+cmd:option('-load_prev', 1)
 local opt = cmd:parse(arg)
-local jsn = utils.read_json(opt.json)
 
 local first_iter = 0
 local val_num = 0
@@ -28,17 +27,36 @@ function pv(i, v)
   end
 end
 
-if opt.out == 'valloss' then
-  local iter = jsn.val_loss_history_it
-  local loss = jsn.val_loss_history
-  local k,v
-  for k,v in pairs(iter) do
-    pv(v,loss[k])
-  end
-elseif opt.out == 'trainloss' then
-  local loss = jsn.train_loss_history
-  for k,v in pairs(loss) do
-    pv(k,v)
+local jsons = {}
+
+function load_jsons(name)
+  --print('Loading json ' .. name)
+  local jsn = utils.read_json(name)
+  if opt.load_prev == 1 and jsn.opt.init_from and jsn.opt.init_from ~= '' then load_jsons(jsn.opt.init_from:gsub(".t7", ".json")) end
+  --print('Loaded json ' .. name)
+  table.insert(jsons, jsn)
+end
+
+--local jsn = utils.read_json(opt.json)
+load_jsons(opt.json)
+
+local ji,jsn
+local iter = 1
+for ji,jsn in ipairs(jsons) do
+  --print('printing json ' .. ji)
+  if opt.out == 'valloss' then
+    local iter = jsn.val_loss_history_it
+    local loss = jsn.val_loss_history
+    local k,v,j
+    for k,v in pairs(iter) do
+      pv(v,loss[k])
+    end
+  elseif opt.out == 'trainloss' then
+    local loss = jsn.train_loss_history
+    for k,v in pairs(loss) do
+      pv(iter,v)
+      iter = iter + 1
+    end
   end
 end
 
