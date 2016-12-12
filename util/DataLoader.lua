312d431fa00b1b69d9d86a3e541d9bf78bf6af95
Michal Povinsky <antihutka@gmail.com>
2016-12-12 11:42:30 +0100

    Add seq_offset

--- util/DataLoader.lua
+++ util/DataLoader.lua
@@ -10,6 +10,7 @@ function DataLoader:__init(kwargs)
   local h5_file = utils.get_kwarg(kwargs, 'input_h5')
   self.batch_size = utils.get_kwarg(kwargs, 'batch_size')
   self.seq_length = utils.get_kwarg(kwargs, 'seq_length')
+  self.seq_offset = utils.get_kwarg(kwargs, 'seq_offset')
   local N, T = self.batch_size, self.seq_length
 
   -- Just slurp all the data into memory
@@ -18,6 +19,10 @@ function DataLoader:__init(kwargs)
   splits.train = f:read('/train'):all()
   splits.val = f:read('/val'):all()
   splits.test = f:read('/test'):all()
+
+  local hb = math.floor(self.batch_size/2)
+  splits.train_off = splits.train[{{1+hb, -1}}]
+  splits.train = splits.train[{{1, -1-hb}}]
 
   self.x_splits = {}
   self.y_splits = {}
@@ -51,6 +56,11 @@ function DataLoader:nextBatch(split)
   local y = self.y_splits[split][idx]
   if idx == self.split_sizes[split] then
     self.split_idxs[split] = 1
+    if split == 'train' and self.seq_offset > 0 then
+      local tx, ty = self.x_splits.train, self.y_splits.train
+      self.x_splits.train, self.y_splits.train = self.x_splits.train_off, self.y_splits.train_off
+      self.x_splits.train_off, self.y_splits.train_off = tx, ty
+    end
   else
     self.split_idxs[split] = idx + 1
   end
