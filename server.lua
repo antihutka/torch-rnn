9f3f4cc83741d5a675a151df1569572714bebdb4
Michal Povinsky <antihutka@gmail.com>
2017-01-26 10:23:06 +0100

    server.lua: add verbose and color options

--- server.lua
+++ server.lua
@@ -15,13 +15,15 @@ cmd:option('-maxlength', 512)
 cmd:option('-interactive', 0)
 cmd:option('-autoreply', 0)
 cmd:option('-start_text', '')
+cmd:option('-verbose', 0)
+cmd:option('-color', 0)
 local opt = cmd:parse(arg)
 
 local checkpoint = torch.load(opt.checkpoint)
 local model = checkpoint.model
-local next_idx = 1
 local input = torch.LongTensor(1, 1)
 local nextline
+local current_scores
 --local output = torch.LongTensor(1, 1)
 
 if opt.interactive == 1 then
@@ -56,28 +58,53 @@ function put_str(s)
   local length = x:size(2)
   local sampled = torch.LongTensor(1, length)
   sampled[{{}, {1, length}}]:copy(x)
-  local scores = model:forward(x)[{{}, {length, length}}]
-  next_idx = model:sampleFromScores(scores, opt.temperature, opt.sample)
+  current_scores = model:forward(x)[{{}, {length, length}}]
+end
+
+function sq(s)
+  io.write('\027[' .. s .. 'm')
+end
+
+function set_color(color)
+  if opt.color > 0 then
+    local code
+    if color == nil then sq(0)
+    elseif color < 0.1 then sq(34) -- blue
+    elseif color < 0.2 then sq(1) sq(34) -- bright blue
+    elseif color < 0.4 then sq(32) -- green
+    elseif color < 0.8 then sq(31) -- red
+    elseif color < 1.6 then sq(1) sq(33) -- bright yellow
+    elseif color < 3.2 then  sq(1) -- bright
+    else sq(1) sq(4) end -- bright underlined
+  end
 end
 
 function get_str()
-  local next_char
+  local next_char, next_idx, next_ent
+  local total_ent, length = 0, opt.maxlength
   for t = 1, opt.maxlength do
+    next_idx, next_ent = model:sampleFromScores(current_scores, opt.temperature, opt.sample)
+    total_ent = total_ent + next_ent
     next_char = model.idx_to_token[next_idx]
+    set_color(next_ent)
     io.write(next_char)
+    set_color()
     input[1] = next_idx
-    local out = model:forward(input)
-    next_idx = model:sampleFromScores(out, opt.temperature, opt.sample)
+    current_scores = model:forward(input)
     if next_char == "\n" then
+      length = t
       break
     end
   end
   if next_char ~= "\n" then io.write('\n') end
+  if opt.verbose > 0 then
+    print(string.format('len %d ent %f', length, total_ent/length))
+  end
 end
 
 if opt.interactive == 1 then io.stdout:setvbuf('no') else io.stdout:setvbuf('line') end
 
-if opt.start_text ~= '' then put_str(opt.start_text .. "\n") end
+put_str(opt.start_text .. "\n")
 
 while true do
   local line = nextline()
